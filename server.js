0cae83c5125098c57b7de4a977baad87fcd0eb96
patricia <garnierpatricia51@gmail.com>
2025-08-28 18:24:35 +0200

    bad

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-//Mes dependance requises
-
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
 require("dotenv").config();
 
-//
 const GetSolde = require("./src/router/RouterGetSolde");
 const GetHistorique = require("./src/router/RouterGetHistorique");
 const GetBeneficiaire = require("./src/router/RouterGetBeneficiaire");
@@ -15,21 +12,16 @@ const PostVirement = require("./src/router/RouterPostVirement");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Mes dependences utiliser
 app.use(cors());
 app.use(bodyParser.json());
 
-// Mes Routes Gets
 app.get("/TableSolde", GetSolde);
 app.get("/TableHistorique", GetHistorique);
 app.get("/TableBeneficiaire", GetBeneficiaire);
 
-//Mes Routes Post
-
 app.post("/ElementBenef", PostBeneficiaire);
 app.post("/virementBenef", PostVirement);
 
-// Lancement du server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
